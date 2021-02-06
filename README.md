4a817cf62d5a57fb341bd50b8411687354f7e58f
michellefitzpatrick <78610909+michellefitzpatrick@users.noreply.github.com>
2021-02-06 13:39:47 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+Data Journalism using d3.js:
+
+A project centered around data-driven storytelling and interactive data visualization. Uses HTML, JavaScript and d3.js to visualizations concerning health markers and poverty levels, to assess the health risks facing particular demographics. Data was presented as a CSV from the U.S. Census Bureau and the Behavioral Risk Factor Surveillance System.
+
+Please visit https://fitzpatrickmn.github.io/Fitzpatrickmn-D3_/ for full display. 
+Feel free to play around with the indicators as the diplay is interactive.
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+To visualize different data segments, please select from the various headings of Poverty (%), Age (Median), Household Income (Median), Obesity (%), Smokers (%), and Without Healthcare (%). Each selection shifts the State data represented in circles on the graph. The circles themselves can be hovered over to view the specific values for that state.
+
+Findings: Through this visualization we are able to piece apart the positive correlation between poverty and healthcare. Not only do we see the general upward trend, but when you place a cursor over a state like Texas (TX), we see a high percentage of the population has no healthcare, 24.9%, and that 17.4% of the population lives in poverty. In contrast, we see states such as Massachusetts (MA), that have a lower percentage of their population without healthcare, 4.6%, also have a lower poverty rate, 11.6%.
+
+When displaying data on healthcare and household income we can observe a negative correlation between median household income and the no healthcare rate. States with higher median imcomes have a lower percentage of their population without healthcare, while the opposite is true for those states with lower median income rates. From our previous example, the state of Massachusetts (MA), which has a median household income of $69,160, has a low no healthcare rate of 4.6%. Mississippi (MS), on the other hand, has a median household income of $39,680, and 18.8% of their population has no healthcare insurance.
+
+As you toggle the axes, you will also uncover trends in how Household Income and Obesity rates impact eachother, how Smoking rates vary with age and household income, and more.
---
+++ assets/js/app.js
@@ -0,0 +1,346 @@
+// Set up SVG definitions
+let svgWidth = 960;
+let svgHeight = 620;
+
+// set up borders in svg
+let margin = {
+    top: 20,
+    right: 40,
+    bottom: 200,
+    left: 100
+};
+
+// calculate chart height and width
+let width = svgWidth - margin.right - margin.left;
+let height = svgHeight - margin.top - margin.bottom;
+
+// append a div class to the scatter element
+let chart = d3.select('#scatter')
+    .append('div')
+    .classed('chart', true);
+
+//append an svg element to the chart 
+let svg = chart.append('svg')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
+
+//append an svg group
+let chartGroup = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+//initial parameters; x and y axis
+let chosenXAxis = 'clickRate';
+let chosenYAxis = 'CRpClick';
+
+//a function for updating the x-scale variable upon click of label
+function xScale(censusData, chosenXAxis) {
+    //scales
+    let xLinearScale = d3.scaleLinear()
+        .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.8,
+        d3.max(censusData, d => d[chosenXAxis]) * 1.2])
+        .range([0, width]);
+
+    return xLinearScale;
+}
+//a function for updating y-scale variable upon click of label
+function yScale(censusData, chosenYAxis) {
+    //scales
+    let yLinearScale = d3.scaleLinear()
+        .domain([d3.min(censusData, d => d[chosenYAxis]) * 0.8,
+        d3.max(censusData, d => d[chosenYAxis]) * 1.2])
+        .range([height, 0]);
+
+    return yLinearScale;
+}
+//a function for updating the xAxis upon click
+function renderXAxis(newXScale, xAxis) {
+    let bottomAxis = d3.axisBottom(newXScale);
+
+    xAxis.transition()
+        .duration(2000)
+        .call(bottomAxis);
+
+    return xAxis;
+}
+
+//function used for updating yAxis variable upon click
+function renderYAxis(newYScale, yAxis) {
+    var leftAxis = d3.axisLeft(newYScale);
+
+    yAxis.transition()
+        .duration(2000)
+        .call(leftAxis);
+
+    return yAxis;
+}
+
+//a function for updating the circles with a transition to new circles 
+function renderCircles(circlesGroup, newXScale, chosenXAxis, newYScale, chosenYAxis) {
+
+    circlesGroup.transition()
+        .duration(2000)
+        .attr('cx', data => newXScale(data[chosenXAxis]))
+        .attr('cy', data => newYScale(data[chosenYAxis]))
+
+    return circlesGroup;
+}
+
+//function for updating STATE labels
+function renderText(textGroup, newXScale, chosenXAxis, newYScale, chosenYAxis) {
+
+    textGroup.transition()
+        .duration(2000)
+        .attr('x', d => newXScale(d[chosenXAxis]))
+        .attr('y', d => newYScale(d[chosenYAxis]));
+
+    return textGroup
+}
+//function to stylize x-axis values for tooltips
+function styleX(value, chosenXAxis) {
+
+    //style based on variable
+    //Click Rate
+    if (chosenXAxis === 'clickRate') {
+        return `${value}%`;
+    }
+}
+
+//funtion for updating circles group
+function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup) {
+
+    //X labels
+    if (chosenXAxis === 'clickRate') {
+        var xLabel = 'Click Rate:';
+    }
+
+    //Y labels
+    if (chosenYAxis === 'CRpClick') {
+        var yLabel = "Conversion Rate per Click:"
+    }
+    else if (chosenYAxis === 'attractivenessRate') {
+        var yLabel = 'Attractiveness Rate:';
+    }
+    else if (chosenYAxis === 'exposureRate') {
+        var yLabel = 'Exposure Rate:';
+    }
+    else {
+        var yLabel = 'Time Before First Click (s):';
+    }
+
+    //create tooltip
+    var toolTip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-8, 0])
+        .html(function (d) {
+            return (`${d.state}<br>${xLabel} ${styleX(d[chosenXAxis], chosenXAxis)}<br>${yLabel} ${d[chosenYAxis]}%`);
+        });
+
+    circlesGroup.call(toolTip);
+
+    //add
+    circlesGroup.on('mouseover', toolTip.show)
+        .on('mouseout', toolTip.hide);
+
+    return circlesGroup;
+}
+//retrieve data
+d3.csv('./assets/data/data.csv').then(function (censusData) {
+
+    console.log(censusData);
+
+    //Parse data
+    censusData.forEach(function (data) {
+        data.attractivenessRate = +data.attractivenessRate;
+        data.timeBeforeFirstClick = +data.timeBeforeFirstClick;
+        data.exposureRate = +data.exposureRate;
+        data.CRpClick = +data.CRpClick;
+        data.clickRate = +data.clickRate;
+    });
+
+    //create linear scales
+    var xLinearScale = xScale(censusData, chosenXAxis);
+    var yLinearScale = yScale(censusData, chosenYAxis);
+
+    //create x axis
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    //append X
+    var xAxis = chartGroup.append('g')
+        .classed('x-axis', true)
+        .attr('transform', `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    //append Y
+    var yAxis = chartGroup.append('g')
+        .classed('y-axis', true)
+        //.attr
+        .call(leftAxis);
+
+    //append Circles
+    var circlesGroup = chartGroup.selectAll('circle')
+        .data(censusData)
+        .enter()
+        .append('circle')
+        .classed('stateCircle', true)
+        .attr('cx', d => xLinearScale(d[chosenXAxis]))
+        .attr('cy', d => yLinearScale(d[chosenYAxis]))
+        .attr('r', 18)
+        .attr('opacity', '.5');
+
+    //append Initial Text
+    var textGroup = chartGroup.selectAll('.stateText')
+        .data(censusData)
+        .enter()
+        .append('text')
+        .classed('stateText', true)
+        .attr('x', d => xLinearScale(d[chosenXAxis]))
+        .attr('y', d => yLinearScale(d[chosenYAxis]))
+        .attr('dy', 3)
+        .attr('font-size', '10px')
+        .text(function (d) { return d.abbr });
+
+    //create a group for the x axis labels
+    var xLabelsGroup = chartGroup.append('g')
+        .attr('transform', `translate(${width / 2}, ${height + 10 + margin.top})`);
+
+    var clickRateLabel = xLabelsGroup.append('text')
+        .classed('aText', true)
+        .classed('active', true)
+        .attr('x', 0)
+        .attr('y', 20)
+        .attr('value', 'clickRate')
+        .text('Click Rate (%)');
+
+    //create a group for Y labels
+    var yLabelsGroup = chartGroup.append('g')
+        .attr('transform', `translate(${0 - margin.left / 4}, ${height / 2})`);
+
+    var CRpClickLabel = yLabelsGroup.append('text')
+        .classed('aText', true)
+        .classed('active', true)
+        .attr('x', 0)
+        .attr('y', 0 - 20)
+        .attr('dy', '1em')
+        .attr('transform', 'rotate(-90)')
+        .attr('value', 'CRpClick')
+        .text('Conversion Rate per Click (%)');
+
+    var exposureLabel = yLabelsGroup.append('text')
+        .classed('aText', true)
+        .classed('inactive', true)
+        .attr('x', 0)
+        .attr('y', 0 - 40)
+        .attr('dy', '1em')
+        .attr('transform', 'rotate(-90)')
+        .attr('value', 'exposureRate')
+        .text('Exposure Rate (%)');
+
+    var arLabel = yLabelsGroup.append('text')
+        .classed('aText', true)
+        .classed('inactive', true)
+        .attr('x', 0)
+        .attr('y', 0 - 60)
+        .attr('dy', '1em')
+        .attr('transform', 'rotate(-90)')
+        .attr('value', 'attractivenessRate')
+        .text('Attractiveness Rate (%)');
+
+    var timeLabel = yLabelsGroup.append('text')
+        .classed('aText', true)
+        .classed('inactive', true)
+        .attr('x', 0)
+        .attr('y', 0 - 80)
+        .attr('dy', '1em')
+        .attr('transform', 'rotate(-90)')
+        .attr('value', 'attractivenessRate')
+        .text('Attractiveness Rate (%)');
+
+    //update the toolTip
+    var circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
+
+    //x axis event listener
+    xLabelsGroup.selectAll('text')
+        .on('click', function () {
+            var value = d3.select(this).attr('value');
+
+            if (value != chosenXAxis) {
+
+                //replace chosen x with a value
+                chosenXAxis = value;
+
+                //update x for new data
+                xLinearScale = xScale(censusData, chosenXAxis);
+
+                //update x 
+                xAxis = renderXAxis(xLinearScale, xAxis);
+
+                //upate circles with a new x value
+                circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis, yLinearScale, chosenYAxis);
+
+                //update text 
+                textGroup = renderText(textGroup, xLinearScale, chosenXAxis, yLinearScale, chosenYAxis);
+
+                //update tooltip
+                circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
+
+                //change of classes changes text
+                if (chosenXAxis === 'clickRate') {
+                    clickRateLabel.classed('active', true).classed('inactive', false);
+                }
+            }
+        });
+    //y axis lables event listener
+    yLabelsGroup.selectAll('text')
+        .on('click', function () {
+            var value = d3.select(this).attr('value');
+
+            if (value != chosenYAxis) {
+                //replace chosenY with value  
+                chosenYAxis = value;
+
+                //update Y scale
+                yLinearScale = yScale(censusData, chosenYAxis);
+
+                //update Y axis 
+                yAxis = renderYAxis(yLinearScale, yAxis);
+
+                //Udate CIRCLES with new y
+                circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis, yLinearScale, chosenYAxis);
+
+                //update TEXT with new Y values
+                textGroup = renderText(textGroup, xLinearScale, chosenXAxis, yLinearScale, chosenYAxis);
+
+                //update tooltips
+                circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
+
+                //Change of the classes changes text
+                if (chosenYAxis === 'attractivenessRate') {
+                    arLabel.classed('active', true).classed('inactive', false);
+                    timeLabel.classed('active', false).classed('inactive', true);
+                    CRpClickLabel.classed('active', false).classed('inactive', true);
+                    exposureLabel.classed('active', false).classed('inactive', true);
+                }
+                else if (chosenYAxis === 'timeBeforeFirstClick') {
+                    arLabel.classed('active', false).classed('inactive', true);
+                    timeLabel.classed('active', true).classed('inactive', false);
+                    CRpClickLabel.classed('active', false).classed('inactive', true);
+                    exposureLabel.classed('active', false).classed('inactive', true);
+
+                }
+                else if (chosenYAxis === 'exposureRate') {
+                    arLabel.classed('active', false).classed('inactive', true);
+                    timeLabel.classed('active', false).classed('inactive', true);
+                    CRpClickLabel.classed('active', false).classed('inactive', true);
+                    exposureLabel.classed('active', true).classed('inactive', false);
+
+                }
+                else {
+                    arLabel.classed('active', false).classed('inactive', true);
+                    timeLabel.classed('active', false).classed('inactive', true);
+                    CRpClickLabel.classed('active', true).classed('inactive', false);
+                    exposureLabel.classed('active', false).classed('inactive', true);
+                }
+            }
+        });
+});
